38d207793fbadb36bac9de4b77ae3bfabe0755fd
FMora15 <pepepinipollo777@gmail.com>
2026-04-25 20:21:57 -0600

    menu principal
    funcionalidad total del menu?

---
+++ build/CMakeFiles/SmartRobot.dir/cmake_clean.cmake
@@ -0,0 +1,31 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SmartRobot.dir/Confimapa.cpp.obj"
+  "CMakeFiles/SmartRobot.dir/Confimapa.cpp.obj.d"
+  "CMakeFiles/SmartRobot.dir/Divide_y_venceras.cpp.obj"
+  "CMakeFiles/SmartRobot.dir/Divide_y_venceras.cpp.obj.d"
+  "CMakeFiles/SmartRobot.dir/Main.cpp.obj"
+  "CMakeFiles/SmartRobot.dir/Main.cpp.obj.d"
+  "CMakeFiles/SmartRobot.dir/imgui/backends/imgui_impl_glfw.cpp.obj"
+  "CMakeFiles/SmartRobot.dir/imgui/backends/imgui_impl_glfw.cpp.obj.d"
+  "CMakeFiles/SmartRobot.dir/imgui/backends/imgui_impl_opengl3.cpp.obj"
+  "CMakeFiles/SmartRobot.dir/imgui/backends/imgui_impl_opengl3.cpp.obj.d"
+  "CMakeFiles/SmartRobot.dir/imgui/imgui.cpp.obj"
+  "CMakeFiles/SmartRobot.dir/imgui/imgui.cpp.obj.d"
+  "CMakeFiles/SmartRobot.dir/imgui/imgui_draw.cpp.obj"
+  "CMakeFiles/SmartRobot.dir/imgui/imgui_draw.cpp.obj.d"
+  "CMakeFiles/SmartRobot.dir/imgui/imgui_tables.cpp.obj"
+  "CMakeFiles/SmartRobot.dir/imgui/imgui_tables.cpp.obj.d"
+  "CMakeFiles/SmartRobot.dir/imgui/imgui_widgets.cpp.obj"
+  "CMakeFiles/SmartRobot.dir/imgui/imgui_widgets.cpp.obj.d"
+  "CMakeFiles/SmartRobot.dir/menu/Menu.cpp.obj"
+  "CMakeFiles/SmartRobot.dir/menu/Menu.cpp.obj.d"
+  "SmartRobot.exe"
+  "SmartRobot.exe.manifest"
+  "SmartRobot.pdb"
+  "libSmartRobot.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/SmartRobot.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
