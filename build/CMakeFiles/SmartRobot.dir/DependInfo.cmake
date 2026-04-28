4afd9a0c7844b251fbe3145fb60f19d88e52d691
FMora15 <pepepinipollo777@gmail.com>
2026-04-27 19:52:40 -0600

    arreglo

--- build/CMakeFiles/SmartRobot.dir/DependInfo.cmake
+++ build/CMakeFiles/SmartRobot.dir/DependInfo.cmake
@@ -8,16 +8,16 @@ set(CMAKE_DEPENDS_LANGUAGES
 
 # The set of dependency files which are needed:
 set(CMAKE_DEPENDS_DEPENDENCY_FILES
-  "C:/Users/lboni/OneDrive/Documentos/robot entrega datos/DeliveryRobot/imgui/backends/imgui_impl_glfw.cpp" "CMakeFiles/SmartRobot.dir/imgui/backends/imgui_impl_glfw.cpp.obj" "gcc" "CMakeFiles/SmartRobot.dir/imgui/backends/imgui_impl_glfw.cpp.obj.d"
-  "C:/Users/lboni/OneDrive/Documentos/robot entrega datos/DeliveryRobot/imgui/backends/imgui_impl_opengl3.cpp" "CMakeFiles/SmartRobot.dir/imgui/backends/imgui_impl_opengl3.cpp.obj" "gcc" "CMakeFiles/SmartRobot.dir/imgui/backends/imgui_impl_opengl3.cpp.obj.d"
-  "C:/Users/lboni/OneDrive/Documentos/robot entrega datos/DeliveryRobot/imgui/imgui.cpp" "CMakeFiles/SmartRobot.dir/imgui/imgui.cpp.obj" "gcc" "CMakeFiles/SmartRobot.dir/imgui/imgui.cpp.obj.d"
-  "C:/Users/lboni/OneDrive/Documentos/robot entrega datos/DeliveryRobot/imgui/imgui_draw.cpp" "CMakeFiles/SmartRobot.dir/imgui/imgui_draw.cpp.obj" "gcc" "CMakeFiles/SmartRobot.dir/imgui/imgui_draw.cpp.obj.d"
-  "C:/Users/lboni/OneDrive/Documentos/robot entrega datos/DeliveryRobot/imgui/imgui_tables.cpp" "CMakeFiles/SmartRobot.dir/imgui/imgui_tables.cpp.obj" "gcc" "CMakeFiles/SmartRobot.dir/imgui/imgui_tables.cpp.obj.d"
-  "C:/Users/lboni/OneDrive/Documentos/robot entrega datos/DeliveryRobot/imgui/imgui_widgets.cpp" "CMakeFiles/SmartRobot.dir/imgui/imgui_widgets.cpp.obj" "gcc" "CMakeFiles/SmartRobot.dir/imgui/imgui_widgets.cpp.obj.d"
-  "C:/Users/lboni/OneDrive/Documentos/robot entrega datos/DeliveryRobot/menu/Menu.cpp" "CMakeFiles/SmartRobot.dir/menu/Menu.cpp.obj" "gcc" "CMakeFiles/SmartRobot.dir/menu/Menu.cpp.obj.d"
-  "C:/Users/lboni/OneDrive/Documentos/robot entrega datos/DeliveryRobot/menu/MenuMain.cpp" "CMakeFiles/SmartRobot.dir/menu/MenuMain.cpp.obj" "gcc" "CMakeFiles/SmartRobot.dir/menu/MenuMain.cpp.obj.d"
-  "C:/Users/lboni/OneDrive/Documentos/robot entrega datos/DeliveryRobot/pruebas/scr/Confimapa.cpp" "CMakeFiles/SmartRobot.dir/pruebas/scr/Confimapa.cpp.obj" "gcc" "CMakeFiles/SmartRobot.dir/pruebas/scr/Confimapa.cpp.obj.d"
-  "C:/Users/lboni/OneDrive/Documentos/robot entrega datos/DeliveryRobot/pruebas/scr/algortimos/Dividevenceras.cpp" "CMakeFiles/SmartRobot.dir/pruebas/scr/algortimos/Dividevenceras.cpp.obj" "gcc" "CMakeFiles/SmartRobot.dir/pruebas/scr/algortimos/Dividevenceras.cpp.obj.d"
+  "C:/Users/fbarr/OneDrive/Documentos/GitHub/DeliveryRobot/imgui/backends/imgui_impl_glfw.cpp" "CMakeFiles/SmartRobot.dir/imgui/backends/imgui_impl_glfw.cpp.obj" "gcc" "CMakeFiles/SmartRobot.dir/imgui/backends/imgui_impl_glfw.cpp.obj.d"
+  "C:/Users/fbarr/OneDrive/Documentos/GitHub/DeliveryRobot/imgui/backends/imgui_impl_opengl3.cpp" "CMakeFiles/SmartRobot.dir/imgui/backends/imgui_impl_opengl3.cpp.obj" "gcc" "CMakeFiles/SmartRobot.dir/imgui/backends/imgui_impl_opengl3.cpp.obj.d"
+  "C:/Users/fbarr/OneDrive/Documentos/GitHub/DeliveryRobot/imgui/imgui.cpp" "CMakeFiles/SmartRobot.dir/imgui/imgui.cpp.obj" "gcc" "CMakeFiles/SmartRobot.dir/imgui/imgui.cpp.obj.d"
+  "C:/Users/fbarr/OneDrive/Documentos/GitHub/DeliveryRobot/imgui/imgui_draw.cpp" "CMakeFiles/SmartRobot.dir/imgui/imgui_draw.cpp.obj" "gcc" "CMakeFiles/SmartRobot.dir/imgui/imgui_draw.cpp.obj.d"
+  "C:/Users/fbarr/OneDrive/Documentos/GitHub/DeliveryRobot/imgui/imgui_tables.cpp" "CMakeFiles/SmartRobot.dir/imgui/imgui_tables.cpp.obj" "gcc" "CMakeFiles/SmartRobot.dir/imgui/imgui_tables.cpp.obj.d"
+  "C:/Users/fbarr/OneDrive/Documentos/GitHub/DeliveryRobot/imgui/imgui_widgets.cpp" "CMakeFiles/SmartRobot.dir/imgui/imgui_widgets.cpp.obj" "gcc" "CMakeFiles/SmartRobot.dir/imgui/imgui_widgets.cpp.obj.d"
+  "C:/Users/fbarr/OneDrive/Documentos/GitHub/DeliveryRobot/menu/Menu.cpp" "CMakeFiles/SmartRobot.dir/menu/Menu.cpp.obj" "gcc" "CMakeFiles/SmartRobot.dir/menu/Menu.cpp.obj.d"
+  "C:/Users/fbarr/OneDrive/Documentos/GitHub/DeliveryRobot/menu/MenuMain.cpp" "CMakeFiles/SmartRobot.dir/menu/MenuMain.cpp.obj" "gcc" "CMakeFiles/SmartRobot.dir/menu/MenuMain.cpp.obj.d"
+  "C:/Users/fbarr/OneDrive/Documentos/GitHub/DeliveryRobot/pruebas/scr/Confimapa.cpp" "CMakeFiles/SmartRobot.dir/pruebas/scr/Confimapa.cpp.obj" "gcc" "CMakeFiles/SmartRobot.dir/pruebas/scr/Confimapa.cpp.obj.d"
+  "C:/Users/fbarr/OneDrive/Documentos/GitHub/DeliveryRobot/pruebas/scr/algortimos/Dividevenceras.cpp" "CMakeFiles/SmartRobot.dir/pruebas/scr/algortimos/Dividevenceras.cpp.obj" "gcc" "CMakeFiles/SmartRobot.dir/pruebas/scr/algortimos/Dividevenceras.cpp.obj.d"
   )
 
 # Targets to which this target links which contain Fortran sources.
