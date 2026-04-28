53cbbb7e26b458d4bcfd06511ff7a65367992220
notminervayet <l.bonilla.2@estudiantec.cr>
2026-04-27 19:06:43 -0600

    rodrigo c
    chaves x laura fernandes

---
+++ build/CMakeFiles/SmartRobot.dir/cmake_clean.cmake
@@ -0,0 +1,31 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SmartRobot.dir/imgui/backends/imgui_impl_glfw.cpp.obj"
+  "CMakeFiles/SmartRobot.dir/imgui/backends/imgui_impl_glfw.cpp.obj.d"
+  "CMakeFiles/SmartRobot.dir/imgui/backends/imgui_impl_opengl3.cpp.obj"
+  "CMakeFiles/SmartRobot.dir/imgui/backends/imgui_impl_opengl3.cpp.obj.d"
+  "CMakeFiles/SmartRobot.dir/imgui/imgui.cpp.obj"
+  "CMakeFiles/SmartRobot.dir/imgui/imgui.cpp.obj.d"
+  "CMakeFiles/SmartRobot.dir/imgui/imgui_draw.cpp.obj"
+  "CMakeFiles/SmartRobot.dir/imgui/imgui_draw.cpp.obj.d"
+  "CMakeFiles/SmartRobot.dir/imgui/imgui_tables.cpp.obj"
+  "CMakeFiles/SmartRobot.dir/imgui/imgui_tables.cpp.obj.d"
+  "CMakeFiles/SmartRobot.dir/imgui/imgui_widgets.cpp.obj"
+  "CMakeFiles/SmartRobot.dir/imgui/imgui_widgets.cpp.obj.d"
+  "CMakeFiles/SmartRobot.dir/menu/Menu.cpp.obj"
+  "CMakeFiles/SmartRobot.dir/menu/Menu.cpp.obj.d"
+  "CMakeFiles/SmartRobot.dir/menu/MenuMain.cpp.obj"
+  "CMakeFiles/SmartRobot.dir/menu/MenuMain.cpp.obj.d"
+  "CMakeFiles/SmartRobot.dir/pruebas/scr/Confimapa.cpp.obj"
+  "CMakeFiles/SmartRobot.dir/pruebas/scr/Confimapa.cpp.obj.d"
+  "CMakeFiles/SmartRobot.dir/pruebas/scr/algortimos/Dividevenceras.cpp.obj"
+  "CMakeFiles/SmartRobot.dir/pruebas/scr/algortimos/Dividevenceras.cpp.obj.d"
+  "SmartRobot.exe"
+  "SmartRobot.exe.manifest"
+  "SmartRobot.pdb"
+  "libSmartRobot.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/SmartRobot.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/SmartRobot.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for SmartRobot.
+# This may be replaced when dependencies are built.
